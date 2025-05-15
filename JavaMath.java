708a52af1eef5d3042c15a55741206d7dbc636a7
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-05-15 11:09:06 +0300

    java math

---
+++ JavaMath.java
@@ -0,0 +1,21 @@
+package Java.JavaMath;
+
+public class JavaMath{
+    public static void main(String[] args){
+        /* The Java Math class has many methods that allows you to perform mathematical tasks on numbers.
+        we have various actions we can perform in the maths class
+        */
+
+        // finding the max number 
+        System.out.println(Math.max(10, 20));
+
+        // finding the min number 
+        System.out.println(Math.min(10, 20));
+        // finding the absolute of a number(converts it into a positive number)
+        System.out.print(Math.abs(-20));
+
+        // generating random numbers
+        System.out.print(Math.floor(Math.random()));
+
+    }
+}
